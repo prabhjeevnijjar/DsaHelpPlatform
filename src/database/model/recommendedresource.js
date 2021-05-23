4f09c124853671f16efa1f8476eb98d25c60b1d6
prabhjeev singh nijjar <prabhjeevnijjar@gmail.com>
2021-05-23 15:06:55 +0530

    database added

---
+++ src/database/model/recommendedresource.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const recommenedresourceSchema = new mongoose.Schema({
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    link: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+},
+    {
+        timestamps: true
+    }
+);
+module.exports = mongoose.model('Recommenedresource', recommenedresourceSchema);
