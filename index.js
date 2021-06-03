7a101643f4b0bb4286eed183e48af4d59914df8a
prabhjeev singh nijjar <prabhjeevnijjar@gmail.com>
2021-06-03 12:47:14 +0530

    cors support added

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ mongoose.connect(
     }
   }
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/api", authh);
 app.use("/homepage", recommend);
 app.use("/homepage", resource);
